ead7c37d05df12119eb6cba91f635898d6017dfa
Michael Kidd <mkidd2515@gmail.com>
2025-02-05 12:13:07 +0000

    Initial commit

--- hello_world.c
+++ hello_world.c
@@ -3,6 +3,6 @@
 
 
 int main(void) {
-  fprintf(stdout, "Hello. My email is:\nmichael.kidd@student.manchester.ac.uk");
+  fprintf(stdout, "Hello. My email address is:\nmichael.kidd@student.manchester.ac.uk");
   return 0;
 }
