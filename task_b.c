ee65083b762958787e861cfafaecc8ea12bf088d
Michael Kidd <mkidd2515@gmail.com>
2025-02-12 11:07:24 +0000

    Initial commit

---
+++ task_b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  double height = 0;
+	double volume = 0;
+	printf("Enter height: ");
+	scanf("%lf", &height);
+	
+	// Your code below here
+  const double radius = 2.2;	
+  //volume = M_PI * radius * radius * height;	
+  
+  volume = M_PI * pow(radius, 2.0) * height;
+	
+	// Do not edit below here
+	printf("Volume: %.2f", volume);
+	return 0;
+}
