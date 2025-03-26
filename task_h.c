bf97afab330ae33312e4a89acabef573d3d13b61
Michael Kidd <mkidd2515@gmail.com>
2025-03-26 11:21:16 +0000

    Initial commit

--- task_h.c
+++ task_h.c
@@ -1,27 +1,33 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 int main(void) {
 
     // Get user input
     #define N 256
-    char buffer[N+1], *filename = buffer;
+    char buffer[N+1], *filename = buffer, *end = buffer;
     char ch;
     int i = 0; // counter for how many characters read
     printf("Enter a file name: ");
     while ((ch = getchar()) != '\n') { // get one character at a time
         filename[i++] = ch;
         // Enter your code under here
-
+        end++;
         
     }
+    
     for (; i < N+1; i++) { // fill the rest of the array with null characters
         filename[i] = '\0';
     }
-    
 
     // Enter your code under here
-
-
+    for (; *end != '.' && end > filename; end--);
+    if (end == filename) {
+      printf("Error!");
+    }
+    else {
+      printf("%s", end);
+    }
 
 
     return EXIT_SUCCESS;
